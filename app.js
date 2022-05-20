30030d08902ee5ea341b1f3a0f66a6ee96cd4dbc
lorecab12 <abogadalorenacabrera@gmail.com>
2022-05-20 19:43:03 -0300

    day3

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ app.get ( '/', ( req , res) => { res.sendFile ( path.resolve ('./views/home.html
 app.listen ( port , () => {console.log ('Servidor corriendo')});
 
 ///app.listen(port, ()=>{console.log(`servidor corriendo en http://localhost:${port}`);});
+
+app.get('/register', (req, res)=> {
+    
+    res.sendFile(__dirname + '/views/register.html');
+
+});
+
+app.get('/login', (req, res)=> {
+    
+    res.sendFile(__dirname + '/views/login.html');
+
+});
