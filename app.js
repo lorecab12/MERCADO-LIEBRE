d45f4f96c40cad5cb4b2c66714ee4e3469412488
lorecab12 <abogadalorenacabrera@gmail.com>
2022-05-26 22:41:39 -0300

    mercado liebre

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log('Servidor funcionando');
 });
 
